0467974e0593e2b65828c08331e5ba8c16a50d6b
iapYang <yklorse@gmail.com>
2017-05-22 14:44:48 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,10 @@ Vue.use(Router);
 export function createRouter () {
     return new Router({
         mode: 'history',
-        routes: [],
+        routes: [{
+            path: '/',
+            component: require('../App.vue'),
+        }],
     });
 }
 
