241d47c1a7646240808a3efe69989f8130a44f68
iapYang <yklorse@gmail.com>
2017-05-22 16:33:26 +0800

    u

--- server.js
+++ server.js
@@ -11,39 +11,39 @@ const renderer = createBundleRenderer(serverBundle, {
     clientManifest,
 });
 
-renderer.renderToString({
-    title: 'Vue HN 2.0', // default title
-    url: 'I',
-}, (err, html) => {
-    if (err) throw err;
-    console.log(html);
+// renderer.renderToString({
+//     title: 'Vue HN 2.0', // default title
+//     url: 'I',
+// }, (err, html) => {
+//     if (err) throw err;
+//     console.log(html);
+// });
+
+server.get('*', (req, res) => {
+    const handleError = err => {
+        if (err && err.code === 404) {
+            res.status(404).end('404 | Page Not Found');
+        } else {
+            // Render Error Page or Redirect
+            res.status(500).end('500 | Internal Server Error');
+            console.error(`error during render : ${req.url}`);
+            console.error(err.stack);
+        }
+    };
+
+    renderer.renderToString({
+        title: 'Vue HN 2.0', // default title
+        url: req.url,
+    }, (err, html) => {
+        if (err) {
+            return handleError(err);
+        }
+        console.log(html);
+        res.end(html);
+    });
 });
 
-// server.get('*', (req, res) => {
-//     const handleError = err => {
-//         if (err && err.code === 404) {
-//             res.status(404).end('404 | Page Not Found');
-//         } else {
-//             // Render Error Page or Redirect
-//             res.status(500).end('500 | Internal Server Error');
-//             console.error(`error during render : ${req.url}`);
-//             console.error(err.stack);
-//         }
-//     };
-
-//     renderer.renderToString({
-//         title: 'Vue HN 2.0', // default title
-//         url: req.url,
-//     }, (err, html) => {
-//         if (err) {
-//             return handleError(err);
-//         }
-//         console.log(html);
-//         res.end(html);
-//     });
-// });
-
-// const port = 8080;
-// server.listen(port, () => {
-//     console.log(`server started at localhost:${port}`);
-// });
+const port = 8080;
+server.listen(port, () => {
+    console.log(`server started at localhost:${port}`);
+});
