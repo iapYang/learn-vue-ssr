2bc45b952f63426b9fe48a2428fc44b05267bbe2
iapYang <yklorse@gmail.com>
2017-05-22 14:12:06 +0800

    reconstruct

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const {createBundleRenderer} = require('vue-server-renderer');
-const Vue = require('vue');
+const server = require('express')();
 
 const template = require('fs').readFileSync('./template.html', 'utf-8');
 const serverBundle = require('./dist/vue-ssr-server-bundle.json');
 const clientManifest = require('./dist/vue-ssr-client-manifest.json');
-
-const app = new Vue({
-    data: {
-        url: 'lol',
-    },
-    template: `<div>The visited URL is: {{ url }}</div>`,
-});
 
 const renderer = createBundleRenderer(serverBundle, {
     template,
     clientManifest,
 });
 
-renderer.renderToString(app, (err, html) => {
+renderer.renderToString(server, (err, html) => {
     if (err) throw err;
     console.log(html);
 });
